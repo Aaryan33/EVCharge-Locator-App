e6846a0a3059c48116b5560545eb5573bcb058b2
Aaryan Patel <97274994+Aaryan33@users.noreply.github.com>
2024-11-25 22:37:20 +0530

    Changes

--- App/Utils/FirebaseConfig.js
+++ App/Utils/FirebaseConfig.js
@@ -1,11 +1,12 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyC1UcH0fdFKffr0Of_kCqDhwHxMIWtWOZE",
   authDomain: "ev-charger-station-edcf5.firebaseapp.com",
